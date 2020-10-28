904c5b3609d0b2b698adca9e468b1264002a2cdf
Srishti Shrivastava <64829430+Srishti2001-eng@users.noreply.github.com>
2020-10-28 18:04:28 +0530

    Add files via upload

---
+++ question5.cpp
@@ -0,0 +1,24 @@
+/*5. (Class Template) Write a program to define a class template for reading
+two data items from the keyboard and find out their sum.*/
+#include <iostream>
+using namespace std;
+ 
+template<class t1,class t2>
+void sum(t1 a,t2 b) 
+{
+    cout<<"\nSum="<<a+b<<endl;
+}
+ 
+int main()
+{
+    int a,b;
+    float x,y;
+    cout<<"\nEnter two integer data: ";
+    cin>>a>>b;
+    cout<<"\nEnter two float data: ";
+    cin>>x>>y;
+    sum(a,b); 
+    sum(x,y); 
+    sum(a,x); 
+    return 0;
+}
